4c316112cc34d7139c3ec563c6cd4e11c48fa36c
Your Name <you@example.com>
2024-09-10 21:29:12 -0700

    Se agrego el proyecto

---
+++ src/styles/Colors.tsx
@@ -0,0 +1,11 @@
+export const myColors = {
+    light: '#f5f5f5',
+    dark: '#4444c2',
+    blue: '#d530bf',
+    btnGray: '#4E505F',
+    btnDark: '#2E2F38',
+    gray: '#747477',
+    black: '#000000',
+    white: '#FFFFFF', 
+    result: '#76b8d8',
+}
